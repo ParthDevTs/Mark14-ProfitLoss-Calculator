1a2303eadc2711e352f202068ce8cb399f16a825
Parth Kathuria <88901443+ParthDevTs@users.noreply.github.com>
2022-08-08 03:40:22 +0530

    added Logic and initial commit

---
+++ mark14-profitloss-calculator/src/App.js
@@ -0,0 +1,102 @@
+import logo from "./logo.svg";
+import "./App.css";
+import { useState } from "react";
+
+function App() {
+  const [stockPrice, setStockPrice] = useState(0);
+  const [quantity, setQuantity] = useState(0);
+  const [currentRate, setCurrentRate] = useState(0);
+  const [message, setMessage] = useState("");
+
+  function stockPriceHandler(event) {
+    setStockPrice(parseInt(event.target.value));
+  }
+
+  function quantityHandler(event) {
+    setQuantity(parseInt(event.target.value));
+  }
+
+  function currentRateHandler(event) {
+    setCurrentRate(parseInt(event.target.value));
+  }
+
+  function setOutputMessage() {
+    if (stockPrice === 0 || quantity === 0 || currentRate === 0) {
+      setMessage("Please fill all the fields and then check Returns.");
+    } else {
+      let totalPrice = stockPrice * quantity;
+      let currentPrice = currentRate * quantity;
+      let change = currentPrice - totalPrice;
+
+      let changePercent = (change / totalPrice) * 100;
+      if (change > 0) {
+        setMessage(
+          "Congratulations! The Profit is ₹" +
+            change +
+            " & Profit% is " +
+            changePercent +
+            "%"
+        );
+      } else if (change === 0) {
+        setMessage("No change from original value, GoodLuck");
+      } else {
+        setMessage(
+          "Oops! Unfortunately you're at a loss of ₹" +
+            change * -1 +
+            " & loss% is " +
+            changePercent * -1 +
+            "%"
+        );
+      }
+
+      console.log(totalPrice, currentPrice, stockPrice, quantity, currentPrice);
+    }
+  }
+
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+
+        <h1>Profit and Loss Calculator</h1>
+      </header>
+      <main className="main">
+        <div className="input__div">
+          <div className="stock__rate input">
+            <label htmlFor="stockRate">Enter buying price of stock</label>
+            <input
+              onChange={stockPriceHandler}
+              type="number"
+              name="stockRate"
+              id="stockRate"
+            />
+          </div>
+          <div className="stock__qty input">
+            <label htmlFor="stockqty">Enter quantity</label>
+            <input
+              onChange={quantityHandler}
+              type="number"
+              name="stockqty"
+              id="stockqty"
+            />
+          </div>
+          <div className="stock__cr_rate input">
+            <label htmlFor="stockcr_rate">
+              Enter the current rate for stock
+            </label>
+            <input
+              onChange={currentRateHandler}
+              type="number"
+              name="stockcr_rate"
+              id="stockcr_rate"
+            />
+          </div>
+          <button onClick={setOutputMessage}>Tell Me!</button>
+        </div>
+        <div className="output">{message}</div>
+      </main>
+    </div>
+  );
+}
+
+export default App;
